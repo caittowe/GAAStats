df8564d874dd24745f4b7539960b5fb6effa3629
40233052 <ctowe01@qub.ac.uk>
2022-08-12 01:25:49 +0100

    timer fixed, scores for p1 shown on stats page

--- app/src/main/java/com/example/statsgaa/EnterStat.java
+++ app/src/main/java/com/example/statsgaa/EnterStat.java
@@ -8,12 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeoutException;
 
 public class EnterStat extends AppCompatActivity {
 
@@ -23,6 +18,8 @@ public class EnterStat extends AppCompatActivity {
     int scoreID = 1;
     int kickpassID = 2;
     int handpassID = 3;
+    String timestamp;
+    int gameID;
 
 
 
@@ -37,7 +34,7 @@ public class EnterStat extends AppCompatActivity {
 
         // first we call this
         getAndSetIntentData();
-
+        gameID = getGameID();
 
         // set actionbar after get and set method
         ActionBar ab = getSupportActionBar();
@@ -49,8 +46,9 @@ public class EnterStat extends AppCompatActivity {
             public void onClick(View view) {
                 MyDatabaseHelper myDB = new MyDatabaseHelper(EnterStat.this);
                 Intent intent = new Intent(EnterStat.this, GameStart.class);
-                myDB.addMatchEvent(scoreID, 1, Integer.parseInt(clickedSquadID), Integer.parseInt(player_no), 2);
+                myDB.addMatchEvent(scoreID, gameID, Integer.parseInt(clickedSquadID), Integer.parseInt(player_no), timestamp);
                 intent.putExtra("clicked_squad_id", toString().valueOf(clickedSquadID));
+                intent.putExtra("gameID", String.valueOf(gameID));
                 startActivity(intent);
                 Log.i("SCOREADDED", "Score added player "+player_no);
             }
@@ -61,13 +59,14 @@ public class EnterStat extends AppCompatActivity {
             public void onClick(View view) {
                 MyDatabaseHelper myDB = new MyDatabaseHelper(EnterStat.this);
                 Intent intent = new Intent(EnterStat.this, GameStart.class);
-                myDB.addMatchEvent(handpassID, 1, Integer.parseInt(clickedSquadID), Integer.parseInt(player_no), 2);
+                myDB.addMatchEvent(handpassID, gameID, Integer.parseInt(clickedSquadID), Integer.parseInt(player_no), timestamp);
                 intent.putExtra("squad_table_id", toString().valueOf(squad_table_id));
                 intent.putExtra("squad_id", toString().valueOf(squad_id));
                 intent.putExtra("squad_name", toString().valueOf(squad_name));
                 intent.putExtra("player_name", toString().valueOf(player_name));
                 intent.putExtra("player_no", toString().valueOf(player_no));
                 intent.putExtra("clicked_squad_id", toString().valueOf(clickedSquadID));
+                intent.putExtra("gameID", toString().valueOf(gameID));
                 startActivity(intent);
                 Log.i("FISTADDED", "Fistpass added player "+player_no);
             }
@@ -78,13 +77,14 @@ public class EnterStat extends AppCompatActivity {
         public void onClick(View view) {
             MyDatabaseHelper myDB = new MyDatabaseHelper(EnterStat.this);
             Intent intent = new Intent(EnterStat.this, GameStart.class);
-            myDB.addMatchEvent(kickpassID, 1, Integer.parseInt(clickedSquadID), Integer.parseInt(player_no), 2);
+            myDB.addMatchEvent(kickpassID, gameID, Integer.parseInt(clickedSquadID), Integer.parseInt(player_no), timestamp);
             intent.putExtra("squad_table_id", toString().valueOf(squad_table_id));
             intent.putExtra("squad_id", toString().valueOf(squad_id));
             intent.putExtra("squad_name", toString().valueOf(squad_name));
             intent.putExtra("player_name", toString().valueOf(player_name));
             intent.putExtra("player_no", toString().valueOf(player_no));
             intent.putExtra("clicked_squad_id", toString().valueOf(clickedSquadID));
+            intent.putExtra("gameID", toString().valueOf(gameID));
             startActivity(intent);
             Log.i("KICKADDED", "kickpass added player "+player_no);
         }
@@ -99,6 +99,7 @@ public class EnterStat extends AppCompatActivity {
         if (getIntent().hasExtra("squad_table_id") && getIntent().hasExtra("squad_id") && getIntent().hasExtra("squad_name") && getIntent().hasExtra("player_name")
         && getIntent().hasExtra("player_no")){
             // getting data from intent
+            timestamp = getIntent().getStringExtra("timestamp");
             squad_table_id = getIntent().getStringExtra("squad_table_id");
             squad_id = getIntent().getStringExtra("squad_id");
             squad_name = getIntent().getStringExtra("squad_name");
@@ -109,6 +110,13 @@ public class EnterStat extends AppCompatActivity {
         } else {
             Toast.makeText(this, "No Data here boss", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public int getGameID(){
+        MyDatabaseHelper myDB = new MyDatabaseHelper(EnterStat.this);
+        gameID = myDB.getMaxGameID();
+        Toast.makeText(this, "gameID = "+gameID, Toast.LENGTH_SHORT).show();
+        return gameID;
     }
 
 
--- app/src/main/java/com/example/statsgaa/ShowGameStats.java
+++ app/src/main/java/com/example/statsgaa/ShowGameStats.java
@@ -1,85 +1,136 @@
 package com.example.statsgaa;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class ShowGameStats extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    MyDatabaseHelper myDB;
-    ArrayList<String> player_id, player_name, player_number, player_scores;
-    CustomAdapterShowStats customAdapter;
 
+    ArrayList<String> playerNames, playerNos, statID;
+    TextView p1Name, p2Name, p3Name, p4Name, p5Name, p6Name, p7Name, p8Name, p9Name, p10Name,
+            p11Name, p12Name, p13Name, p14Name, p15Name, p1Score, p2Score, p3Score, p4Score,
+            p5Score, p6Score, p7Score, p8Score, p9Score, p10Score, p11Score, p12Score, p13Score,
+            p14Score, p15Score;
 
-    /**
-     * displays the details entered from the dialog
-     * calls storedatainarrays method
-     * calls the custom adapter to layout the recycler view of all players
-     *
-     * @param savedInstanceState
-     */
+    String clickedSquadID;
+    String gameID;
+    int p1ScoreCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_game_stats);
 
-        recyclerView = findViewById(R.id.recyclerView);
+        p1Name = findViewById(R.id.p1Name);
+        p2Name = findViewById(R.id.p2Name);
+        p3Name = findViewById(R.id.p3Name);
+        p4Name = findViewById(R.id.p4Name);
+        p5Name = findViewById(R.id.p5Name);
+        p6Name = findViewById(R.id.p6Name);
+        p7Name = findViewById(R.id.p7Name);
+        p8Name = findViewById(R.id.p8Name);
+        p9Name = findViewById(R.id.p9Name);
+        p10Name = findViewById(R.id.p10Name);
+        p11Name = findViewById(R.id.p11Name);
+        p12Name = findViewById(R.id.p12Name);
+        p13Name = findViewById(R.id.p13Name);
+        p14Name = findViewById(R.id.p14Name);
+        p15Name = findViewById(R.id.p15Name);
 
-        myDB = new MyDatabaseHelper(ShowGameStats.this);
-        player_id = new ArrayList<>();
-        player_name = new ArrayList<>();
-        player_number = new ArrayList<>();
-        player_scores = new ArrayList<>();
+        p1Score = findViewById(R.id.p1Score);
+        p2Score = findViewById(R.id.p2Score);
+        p3Score = findViewById(R.id.p3Score);
+        p4Score = findViewById(R.id.p4Score);
+        p5Score = findViewById(R.id.p5Score);
+        p6Score = findViewById(R.id.p6Score);
+        p7Score = findViewById(R.id.p7Score);
+        p8Score = findViewById(R.id.p8Score);
+        p9Score = findViewById(R.id.p9Score);
+        p10Score = findViewById(R.id.p10Score);
+        p11Score = findViewById(R.id.p11Score);
+        p12Score = findViewById(R.id.p12Score);
+        p13Score = findViewById(R.id.p13Score);
+        p14Score = findViewById(R.id.p14Score);
+        p15Score = findViewById(R.id.p15Score);
+
+        getIntentData();
+
+        playerNames = new ArrayList<>();
+        playerNos = new ArrayList<>();
+        statID = new ArrayList<>();
 
         storeDataInArrays();
 
-        customAdapter = new CustomAdapterShowStats(ShowGameStats.this, this, player_id, player_name, player_number, player_scores);
-        recyclerView.setAdapter(customAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(ShowGameStats.this));
+        p1Name.setText(playerNos.get(0) + ". " + playerNames.get(0));
+        p2Name.setText(playerNos.get(1) + ". " + playerNames.get(1));
+        p3Name.setText(playerNos.get(2) + ". " + playerNames.get(2));
+        p4Name.setText(playerNos.get(3) + ". " + playerNames.get(3));
+        p5Name.setText(playerNos.get(4) + ". " + playerNames.get(4));
+        p6Name.setText(playerNos.get(5) + ". " + playerNames.get(5));
+        p7Name.setText(playerNos.get(6) + ". " + playerNames.get(6));
+        p8Name.setText(playerNos.get(7) + ". " + playerNames.get(7));
+        p9Name.setText(playerNos.get(8) + ". " + playerNames.get(8));
+        p10Name.setText(playerNos.get(9) + ". " + playerNames.get(9));
+        p11Name.setText(playerNos.get(10) + ". " + playerNames.get(10));
+        p12Name.setText(playerNos.get(11) + ". " + playerNames.get(11));
+        p13Name.setText(playerNos.get(12) + ". " + playerNames.get(12));
+        p14Name.setText(playerNos.get(13) + ". " + playerNames.get(13));
+        p15Name.setText(playerNos.get(14) + ". " + playerNames.get(14));
 
-        MyDatabaseHelper db = new MyDatabaseHelper(ShowGameStats.this);
-        db.close();
+        storePlayerStatData();
+
+        p1Score.setText(String.valueOf(p1ScoreCount));
+
     }
+
 
     /**
-     * allows results to be seen when data is changed
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
+     * gets match id
      */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            recreate();
+    public void getIntentData() {
+        if (getIntent().hasExtra("squadID") && (getIntent().hasExtra("gameID"))) {
+            clickedSquadID = getIntent().getStringExtra("squadID");
+            gameID = getIntent().getStringExtra("gameID");
+            Log.i("GOTSQUAD", "getIntentData: squad id =  " + clickedSquadID);
+        } else {
+            Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
 
     /**
      * Stores data entered into array lists
      */
     public void storeDataInArrays() {
-        Cursor cursor = myDB.readAllSquadData();
-        if (cursor.getCount() == 0) {
+        MyDatabaseHelper myDB = new MyDatabaseHelper(ShowGameStats.this);
+        Cursor cursorPlayer = myDB.readAllClickedSquadData(clickedSquadID);
+        if (cursorPlayer.getCount() == 0) {
 //           empty_imageview.setVisibility(View.VISIBLE);
 //           no_data.setVisibility(View.VISIBLE);
         } else {
-            while (cursor.moveToNext()) {
-                player_id.add(cursor.getString(0));
-                player_name.add(cursor.getString(1));
-                player_number.add(cursor.getString(2));
-                player_scores.add(cursor.getString(3));
+            while (cursorPlayer.moveToNext()) {
+                playerNames.add(cursorPlayer.getString(3));
+                playerNos.add(cursorPlayer.getString(4));
+
             }
         }
+    }
+
+    public void storePlayerStatData(){
+        MyDatabaseHelper myDB = new MyDatabaseHelper(ShowGameStats.this);
+        p1ScoreCount  = myDB.showPlayerStatsCount("1" ,"1", gameID);
+        Log.i("SCORECOUNT","stat count for p1 = "+p1ScoreCount);
 
     }
+
+
 }
